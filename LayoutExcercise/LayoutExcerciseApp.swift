f1f84f0f84dcf94adbb96cf9a2faf8310adbd0fa
Leo Lu <llu@lcs.on.ca>
2021-10-26 14:17:37 -0400

    Initial Commit

---
+++ LayoutExcercise/LayoutExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExcerciseApp.swift
+//  LayoutExcercise
+//
+//  Created by Leo Lu on 2021-10-26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
